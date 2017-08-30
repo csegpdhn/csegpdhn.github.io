1f86f0dc527b4b61de9e2cf1ada7fed231d33592
csegpdhn <csegpdhn@gmail.com>
2017-08-31 00:00:54 +0530

    Few edits

--- slider.js
+++ slider.js
@@ -1,6 +1,6 @@
  $(document).ready(function(){
   $('.image').slick({
-     infinite: true,
+     infinite: false,
   slidesToShow: 3,
   speed : 200,
   slidesToScroll: 2,
@@ -13,9 +13,9 @@
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: false,
         dots: false
       }
     },
